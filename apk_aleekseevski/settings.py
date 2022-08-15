76e3c2e10c282e35f9286b8785baa272f049c764
Gosha Kokorin <go_2002@mail.ru>
2022-08-15 14:35:15 +0500

    docker

--- apk_aleekseevski/settings.py
+++ apk_aleekseevski/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j$xm34nkg-r#)%ayw952v*bx!3j+_=20vlvzx&haxo$_ue5x!b'
+SECRET_KEY = 'django-insecure-j$xm34nk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
